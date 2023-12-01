5d52352479634cbc51e25480948f469e13310d2a
Ojus Sharma <ojus@student.ubc.ca>
2023-12-01 03:19:18 -0800

    Navigation, Route Info, Incident History added

---
+++ AndroidStudioProjects/project/app/src/main/java/com/example/project/incidentHistory.java
@@ -0,0 +1,20 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class incidentHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incident_history);
+    }
+
+    public void onBack(View view)
+    {
+        finish();
+    }
+}
