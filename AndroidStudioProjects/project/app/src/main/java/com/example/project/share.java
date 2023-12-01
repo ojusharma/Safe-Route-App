03646281febafcd98162228a615a02fb8abc1fc0
mahigangal <gangalmahi@gmail.com>
2023-12-01 08:37:33 -0800

    share page added

--- AndroidStudioProjects/project/app/src/main/java/com/example/project/share.java
+++ AndroidStudioProjects/project/app/src/main/java/com/example/project/share.java
@@ -7,27 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class helpchat extends AppCompatActivity {
+public class share extends AppCompatActivity {
 
     ImageView backk;
     ImageView homee;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_helpchat);
+        setContentView(R.layout.activity_share);
 
         backk=findViewById(R.id.back);
         homee=findViewById(R.id.home);
-
     }
+
     public void onClick1(View v){
         finish();
     }
 
     public void onClick2(View v){
-        Intent intent = new Intent(helpchat.this, homescreen.class);
+        Intent intent = new Intent(share.this, homescreen.class);
 
         startActivity(intent);
 
