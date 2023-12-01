0d02082a4806cc07456dede7cc47d695675d3e58
Sparsh khanna <97553311+Sparshkhannaa@users.noreply.github.com>
2023-12-01 00:04:12 -0800

    worked on user profile
    Designed user profile

--- AndroidStudioProjects/project/app/src/main/java/com/example/project/profile.java
+++ AndroidStudioProjects/project/app/src/main/java/com/example/project/profile.java
@@ -2,13 +2,22 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class profile extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+    }
+    public void GOBACK(View v){
+        finish();
+
+
     }
 }
