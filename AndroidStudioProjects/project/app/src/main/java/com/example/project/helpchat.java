bb1d822e342a54f861c0ba1011283937caff7392
Sparsh khanna <97553311+Sparshkhannaa@users.noreply.github.com>
2023-12-03 20:50:31 -0800

    Chat DOne
    I have coded and completed chat only formatting and a little bit of alignment will finish the class completely !

--- AndroidStudioProjects/project/app/src/main/java/com/example/project/helpchat.java
+++ AndroidStudioProjects/project/app/src/main/java/com/example/project/helpchat.java
@@ -1,78 +1,104 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.project.R;
 
 public class helpchat extends AppCompatActivity {
+    EditText input;
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    TextView t4;
+    TextView t5;
+    TextView t6;
 
-    ImageView backk;
-    ImageView homee;
-    RecyclerView recyclerViewChat;
-    EditText editTextText;
-    Button buttonSend;
-    ChatAdapter chatAdapter;
-    TextView initialMessage;
+    String andar ;
+    ImageView t1p;
+    ImageView t2p;
+    ImageView t3p;
+    ImageView t4p;
+    ImageView t5p;
+    ImageView t6p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpchat);
+        t1 = findViewById(R.id.text1);
+        t2 = findViewById(R.id.text2);
+        t3 = findViewById(R.id.text3);
+        t4 = findViewById(R.id.text4);
+        t1p= findViewById(R.id.imageView);
+        t2p= findViewById(R.id.imageView2);
+        t3p= findViewById(R.id.imageView3);
+        t4p= findViewById(R.id.imageView4);
+        t5 = findViewById(R.id.text5);
+        t6 = findViewById(R.id.text6);
+        t5p =  findViewById(R.id.imageView5);
+        t6p = findViewById(R.id.imageView6);
+        input = findViewById(R.id.editTextTextMultiLine);
+        //t5 here with delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Code to be executed after 2 seconds
+                // For example, displaying a TextView
 
-        backk = findViewById(R.id.back);
-        homee = findViewById(R.id.home);
-        recyclerViewChat = findViewById(R.id.recyclerViewChat);
-        editTextText = findViewById(R.id.editTextText);
-        buttonSend = findViewById(R.id.buttonSend);
-        initialMessage = findViewById(R.id.initialMessage);
+                t5.setText("Hi How Can i help you ?");
+                t5.setBackgroundResource(R.drawable.chat_bubble2);
+                t5p.setImageResource(R.drawable.profile_icon);
 
-        // Display initial message
-        initialMessage.setText("What do you want help with?");
+            }
+        }, 2500);
 
-        // Initialize RecyclerView and ChatAdapter
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerViewChat.setLayoutManager(layoutManager);
 
-        List<Message> messages = new ArrayList<>();
-        chatAdapter = new ChatAdapter(messages);
-        recyclerViewChat.setAdapter(chatAdapter);
     }
+    public void USERTEXTED(View view){
+        andar = input.getText().toString();
 
-    public void onClick1(View v) {
-        finish();
-    }
+        input.setText("");
+        if(andar.length() == 0){
 
-    public void onClick2(View v) {
-        Intent intent = new Intent(helpchat.this, homescreen.class);
-        startActivity(intent);
-    }
+        }
+        else {
+            if (t1.getText().toString().length() == 0) {
+                t1.setText("  " + andar);
+                t1.setBackgroundResource(R.drawable.chat_bubble);
+                t1p.setImageResource(R.drawable.profile_icon);
+            } else if (t2.getText().toString().length() == 0) {
+                t2.setText("  " + andar);
+                t2.setBackgroundResource(R.drawable.chat_bubble);
+                t2p.setImageResource(R.drawable.profile_icon);
 
-    // Handle the send button click
-    public void onSendButtonClick(View view) {
-        String userMessage = editTextText.getText().toString().trim();
-        if (!userMessage.isEmpty()) {
-            // Display user's message in RecyclerView
-            chatAdapter.addMessage(new Message(userMessage, true));
+            } else if (t3.getText().toString().length() == 0) {
+                t3.setText("  " + andar);
+                t3.setBackgroundResource(R.drawable.chat_bubble);
+                t3p.setImageResource(R.drawable.profile_icon);
+            } else {
+                t4.setText("  " + andar);
+                t4.setBackgroundResource(R.drawable.chat_bubble);
+                t4p.setImageResource(R.drawable.profile_icon);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Code to be executed after 2 seconds
+                        // For example, displaying a TextView
 
-            // Simulate a response (replace with your actual logic)
-            String response = "Thank you for your message! I'll get back to you shortly.";
-            chatAdapter.addMessage(new Message(response, false));
+                        t6.setText("Call 911 please !");
+                        t6.setBackgroundResource(R.drawable.chat_bubble2);
+                        t6p.setImageResource(R.drawable.profile_icon);
 
-            // Clear the message input
-            editTextText.setText("");
-
-            // Scroll to the last message
-            recyclerViewChat.smoothScrollToPosition(chatAdapter.getItemCount() - 1);
+                    }
+                }, 2500);
+            }
         }
     }
 }
